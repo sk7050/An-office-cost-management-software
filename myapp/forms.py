2e6a5b9dc12ba3fd9c573a2aae9c3184dabf3bae
Shaikh Abu Faisal <60063407+sk7050@users.noreply.github.com>
2020-12-01 01:00:50 +0600

    Update forms.py

--- myapp/forms.py
+++ myapp/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from .models import user_class
-
+from .models import sold_data
+from datetime import date
 
 
 
@@ -14,17 +15,30 @@ class Office_expense(forms.Form):
    
     expense=forms.CharField(max_length=50)
     cost=forms.FloatField()
-    date=forms.DateField(widget=forms.SelectDateWidget())
+    date=forms.DateField(widget=forms.SelectDateWidget(),initial=date.today)
 
 class Product_cost(forms.Form):
     
     products=forms.CharField(max_length=50)
     measurements=forms.FloatField()
     measurements_type=forms.ChoiceField(choices =[
-        ('kilogram','KG'),
+        ('Kg','KG'),
         ('Liter','LTR'),
         ('Hali','HALI'),
         ('Dozen','DOZEN'),
     ],  initial='KG', widget=forms.Select(), required=True)
-    cost=forms.FloatField()
-    date=forms.DateField(widget=forms.SelectDateWidget())
+    cost_per_measurement=forms.FloatField()
+    date=forms.DateField(widget=forms.SelectDateWidget(),initial=date.today)
+
+class Sold_data(forms.ModelForm):
+    transaction_id=forms.CharField(max_length=50,required=False)
+    sold_date=forms.DateField(widget=forms.SelectDateWidget(),initial=date.today)
+    measurements_type=forms.ChoiceField(choices =[
+        ('Kg','KG'),
+        ('Liter','LTR'),
+        ('Hali','HALI'),
+        ('Dozen','DOZEN'),
+    ],  initial='KG', widget=forms.Select(), required=True)
+    class Meta:
+        model=sold_data
+        fields = ['sold_products','how_many', 'measurements_type','sold_price','transaction_id','sold_date']
