1c264b4e909e207ae60137439987e4e29a866959
Shaikh Abu Faisal <60063407+sk7050@users.noreply.github.com>
2020-12-01 01:03:31 +0600

    Update views.py

--- myapp/views.py
+++ myapp/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render,redirect
 from django.urls import reverse
 
-from .models import user_class,office_expense,product_cost
-from .forms import User_Class,Office_expense,Product_cost
-
+from .models import user_class,office_expense,product_cost,sold_data
+from .forms import User_Class,Office_expense,Product_cost,Sold_data
+from django.contrib.sessions.models import Session
 
 
 
@@ -92,9 +92,9 @@ def Accounts_view(request):
       error=pass_check(password,username)
     
       if error== 1:
-        username=login_user(username)
+        request.session['username'] = username
         password=login_password(password)
-        context=cont(error,form1=Office_expense(),form2=Product_cost(),detail1=user_class.objects.get(name=username),detail2=office_expense.objects.all(),detail3=product_cost.objects.all())
+        context=cont(error,form1=Office_expense(),form2=Product_cost(),form3=Sold_data(),detail1=user_class.objects.get(name=username),detail2=office_expense.objects.all(),detail3=product_cost.objects.all())
         return render(request, 'user.html',context) 
       else:
         context=cont(error,form)
@@ -102,10 +102,14 @@ def Accounts_view(request):
              
   else:  
     try:
+      with open("temp_user.txt",'w',encoding = 'utf-8') as f:
+          f.write(" ")
+      with open("temp_password.txt",'w',encoding = 'utf-8') as f:
+          f.write(" ")
       error=1
       username=login_user()
       #password=login_password()
-      context=cont(error,form1=Office_expense(),form2=Product_cost(),detail1=user_class.objects.get(name=username),detail2=office_expense.objects.all(),detail3=product_cost.objects.all())
+      context=cont(error,form1=Office_expense(),form2=Product_cost(),form3=Sold_data(),detail1=user_class.objects.get(name=username),detail2=office_expense.objects.all(),detail3=product_cost.objects.all())
       return render(request, 'user.html',context)
      
              
@@ -129,13 +133,13 @@ def accounts_view(request):
   
           exobject=office_expense(name_id_id=user_id,expense=name_of_expense,cost=cost_amount,date=date)  
           exobject.save()
-          context=cont(error=0,form1=Office_expense(),form2=Product_cost(),detail1=user_class.objects.get(id=user_id),detail2=office_expense.objects.all(),detail3=product_cost.objects.all())
-          return render(request, 'user.html',context)
+          username=login_user(user_class.objects.get(id=user_id).name)
+          return redirect('/login/accounts/')
         elif request.method == 'GET':
             error=1
             username=login_user()
             #password=login_password()
-            context=cont(error,form1=Office_expense(),form2=Product_cost(),detail1=user_class.objects.get(name=username),detail2=office_expense.objects.all(),detail3=product_cost.objects.all())
+            context=cont(error,form1=Office_expense(),form2=Product_cost(),form3=Sold_data(),detail1=user_class.objects.get(name=username),detail2=office_expense.objects.all(),detail3=product_cost.objects.all())
             return render(request, 'user.html',context)
        
   elif Product_cost(request.POST or None).is_valid():
@@ -144,29 +148,56 @@ def accounts_view(request):
               name_of_products=request.POST['products']
               measurements_name=request.POST['measurements']
               measurements_type=request.POST['measurements_type']
-              cost_amount=request.POST['cost']
+              cost_amount=request.POST['cost_per_measurement']
               month=request.POST['date_month']
               day=request.POST['date_day']
               year=request.POST['date_year']
               date=year+'-'+month+'-'+day
+              total_cost=float(measurements_name)*float(cost_amount)
   
-              probject=product_cost(name_id_id=user_id,products=name_of_products,measurements=measurements_name,measurements_type=measurements_type,cost=cost_amount,date=date)  
+              probject=product_cost(name_id_id=user_id,products=name_of_products,measurements=measurements_name,measurements_type=measurements_type,cost=total_cost,date=date)  
               probject.save()
-              context=cont(error=0,form1=Office_expense(),form2=Product_cost(),detail1=user_class.objects.get(id=user_id),detail2=office_expense.objects.all(),detail3=product_cost.objects.all())
-              return render(request, 'user.html',context) 
-        
+              username=login_user(user_class.objects.get(id=user_id).name)
+              return redirect('/login/accounts/')
         elif request.method == 'GET':
             error=1
             username=login_user()
             #password=login_password()
-            context=cont(error,form1=Office_expense(),form2=Product_cost(),detail1=user_class.objects.get(name=username),detail2=office_expense.objects.all(),detail3=product_cost.objects.all())
+            context=cont(error,form1=Office_expense(),form2=Product_cost(),form3=Sold_data(),detail1=user_class.objects.get(name=username),detail2=office_expense.objects.all(),detail3=product_cost.objects.all())
             return render(request, 'user.html',context)
+
+  elif Sold_data(request.POST or None).is_valid():
+        if request.method == 'POST':
+           
+          #name_of_expense=request.POST['expense']
+          #cost_amount=request.POST['cost']
+          #month=request.POST['date_month']
+          #day=request.POST['date_day']
+          #year=request.POST['date_year']
+          #date=year+'-'+month+'-'+day
+  
+          object=Sold_data(request.POST) 
+          object.save()
+          username=login_user(request.session['username'])
+          
+          return redirect('/login/accounts/')
+        elif request.method == 'GET':
+            error=1
+            username=login_user()
+            #password=login_password()
+            context=cont(error,form1=Office_expense(),form2=Product_cost(),form3=Sold_data(),detail1=user_class.objects.get(name=username),detail2=office_expense.objects.all(),detail3=product_cost.objects.all())
+            return render(request, 'user.html',context)
+        else:
+            print("error")
+            return redirect('/login/')
+  
   else:
     try:
       error=1
       username=login_user()
+      username=login_user(request.session['username'])
       #password=login_password()
-      context=cont(error,form1=Office_expense(),form2=Product_cost(),detail1=user_class.objects.get(name=username),detail2=office_expense.objects.all(),detail3=product_cost.objects.all())
+      context=cont(error,form1=Office_expense(),form2=Product_cost(),form3=Sold_data(),detail1=user_class.objects.get(name=username),detail2=office_expense.objects.all(),detail3=product_cost.objects.all())
       return render(request, 'user.html',context)
      
              
@@ -197,7 +228,7 @@ def  office_expense_edit_view(request,id):
         'cost':obex.cost,
         'date':obex.date,
           }
-  print(data)
+  
   bd=Office_expense(data)
   if request.method=='POST':
     name_of_expense=request.POST['expense']
@@ -207,11 +238,12 @@ def  office_expense_edit_view(request,id):
     year=request.POST['date_year']
     date=year+'-'+month+'-'+day
     office_expense.objects.filter(id=id).update(expense=name_of_expense,cost=cost_amount,date=date)
-    context=cont(error=0,form1=Office_expense(),form2=Product_cost(),detail1=user_class.objects.get(id=user_id),detail2=office_expense.objects.all(),detail3=product_cost.objects.all())
+    username=login_user(user_class.objects.get(id=user_id).name)
+   
     #return redirect('accounts',context) 
-    return redirect('/login')
+    return redirect('/login/accounts/')
     
-  context=cont(form1=bd,detail1=obex)
+  context=cont(form1=bd,detail1=user_class.objects.get(id=user_id))
   return render(request, 'edit.html',context)
   
   
@@ -225,7 +257,7 @@ def product_purchase_edit_view(request,id):
         'cost':obex.cost,
         'date':obex.date,
           }
-  print(data)
+  
   bd=Product_cost(data)
   if request.method=='POST':
       name_of_products=request.POST['products']
@@ -237,14 +269,80 @@ def product_purchase_edit_view(request,id):
       year=request.POST['date_year']
       date=year+'-'+month+'-'+day
   
-      product_cost.objects.filter(id=id).update(products=name_of_products,measurements=measurements_name,measurements_type=measurements_type,cost=cost_amount,date=date)  
+      product_cost.objects.filter(id=id).update(products=name_of_products,measurements=measurements_name,measurements_type=measurements_type,cost=cost_amount,date=date)
+      username=login_user(user_class.objects.get(id=user_id).name)  
       
-      return redirect('/login/accounts')
+      return redirect('/login/accounts/')
   context=cont(form3=bd,detail3=user_class.objects.get(id=user_id))
   return render(request, 'edit.html',context) 
 
+def sold_data_view(request):
+  sell_amount=0
+  for i in sold_data.objects.all():
+    sell_amount += i.sold_price
+  
+  context=cont(detail1=sold_data.objects.all())
+  context['total_sell']=sell_amount 
+  
+  
+  return render(request, 'sold_data.html',context)
 
 
+def office_account_view(request):
+  context={}
+  office_cost=0
+  purchase_cost=0
+  Office_user=[]
+  Employee_name={}
+  for i in user_class.objects.all():
+    Office_user.append(i.name)
+  
+  n=0
+  x=0
+  Expense_by_Office_Account=0
+  
+  for j in Office_user:
+    
+    for i in office_expense.objects.filter(name_id_id=user_class.objects.get(name=j).id):
+      Employee_name.setdefault('office_expense'+'_'+str(i.name_id_id),[])
+      Employee_name['office_expense'+'_'+str(i.name_id_id)].append(i.cost)
+      #of=Employee_name.setdefault('office_expense'+'_'+str(i.name_id_id))
+    for i in product_cost.objects.filter(name_id_id=user_class.objects.get(name=j).id):
+      Employee_name.setdefault('product_cost'+'_'+str(i.name_id_id),[])
+      Employee_name['product_cost'+'_'+str(i.name_id_id)].append(i.cost)
+      #po=Employee_name.setdefault('product_cost'+j)
+  for user in user_class.objects.all():
+    for keys,values in Employee_name.items(): 
+      #if str(user.id) == str(keys.split('_')[1]):
+        n+=1
+        context[keys]=sum(values)
+        context['user'+'_'+str(user.id)]=user_class.objects.get(id=user.id).name
+        context.setdefault('no',[])
+        context['no'].append(n)
+    for k,v in context.items():
+      if(len(k.split('_')))==3:
+        if (user_class.objects.get(id=(k.split('_')[2])).name) =='Office_Account':
+           if 'office_expense_'+str(user.id) in k or 'product_cost_'+str(user.id) in k :
+             
+             Expense_by_Office_Account +=float(v)
+
+      
+      if 'office_expense_'+str(user.id) in k or 'product_cost_'+str(user.id) in k :
+        x+=float(v)
+
+        
+       
+  
+  context['Expense_by_Office_Account']=Expense_by_Office_Account
+  context['Total_cost']=x
+  context["user"]=user_class.objects.all()
+  context['Total_sell']=sum([i.sold_price for i in sold_data.objects.all()])
+  d=context['Total_sell']-context['Total_cost']
+  context['dif']=d
+  context['office_balane']=context['Total_sell']-context['Expense_by_Office_Account']
+  return render(request,'office_account.html',context)
+  
+  
 
       
 
