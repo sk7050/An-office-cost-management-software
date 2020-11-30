90e8b3f4daaf1b29239cf25142f4311d28913552
Shaikh Abu Faisal <60063407+sk7050@users.noreply.github.com>
2020-12-01 00:58:47 +0600

    Update admin.py

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from .models import user_class,office_expense,product_cost
+from .models import user_class,office_expense,product_cost,sold_data
 
 admin.site.register(user_class)
 admin.site.register(office_expense)
 admin.site.register(product_cost)
-
-# Register your models here.
+admin.site.register(sold_data)
